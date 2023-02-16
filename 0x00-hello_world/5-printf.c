040f2f9a04874f03f4f52107779f5e33ccd08edd
FaithfulnessJ <faithfulnessjoshua2001@gmail.com>
2023-02-16 05:06:53 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - A program that ornts a line using a pint f function
+ *return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
+
