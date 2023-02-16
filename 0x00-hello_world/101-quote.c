c2def8687a1c0eda043627177060b103416c2645
FaithfulnessJ <faithfulnessjoshua2001@gmail.com>
2023-02-16 07:14:29 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 1 (Success)
+ * main - A program that prnts a line to standard error
+ * Return: always 1
  */
 int main(void)
 {
-	write(2,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	return (1);
 }
