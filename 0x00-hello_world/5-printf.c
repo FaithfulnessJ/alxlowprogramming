c8449b94ef5e8894cdac51c73333af78766bf4e8
FaithfulnessJ <faithfulnessjoshua2001@gmail.com>
2023-02-16 05:39:37 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
+ * *main - A program that pints a line using printf function
  * Return: always 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return (0);
 }
