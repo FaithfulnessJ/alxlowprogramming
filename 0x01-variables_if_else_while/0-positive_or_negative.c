74b5ccb13c98c4d764e68cb33dfe6ba7a6faabf0
FaithfulnessJ <faithfulnessjoshua2001@gmail.com>
2023-02-17 04:34:41 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,19 +12,19 @@ int main(void)
 {
 int x;
 
-srand(time(0);
+srand(time(0));
 x = rand() - RAND_MAX / 2;
 if (x > 0)
 {
-printf("%d is positive\n", x);
+printf("%d is positive\n",x);
 }
 else if (x == 0)
 {
-printf("%d is zero\n", n);
+printf("%d is zero\n",x);
 }
 else
 {
-printf("%d is negative\n", n);
+printf("%d is negative\n",x);
 }
 return (0);
 }
