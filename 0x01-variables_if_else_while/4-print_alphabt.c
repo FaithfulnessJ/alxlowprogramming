0202508beb41ae683b7441106ae7a9bf5467bcdb
FaithfulnessJ <faithfulnessjoshua2001@gmail.com>
2023-02-17 06:44:33 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@ int main(void)
 char a;
 a = 'g';
 while
-	(a <= 'z') {
-		if ((a != 'q' && a !=  'e') && a <= 'z')
-			putchar(a);
-			a++;
-		}
-	putchar('\n');
+(a <= 'z') {
+if ((a != 'q' && a !=  'e') && a <= 'z')
+putchar(a);
+a++;
+}
+putchar('\n');
 return (0);
 }
