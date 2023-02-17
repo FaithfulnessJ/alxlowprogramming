63e51ce8829058bcebed56540113ea4f03288052
FaithfulnessJ <faithfulnessjoshua2001@gmail.com>
2023-02-17 00:38:08 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base, followed by a a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int a;
+
+a = 0;
+while
+	(a < 10) {
+		putchar(a + '0');
+		a++;
+	}
+putchar('\n');
+return (0);
+}
