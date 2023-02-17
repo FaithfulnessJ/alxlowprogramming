bb4850b26a80e97754fc91b8f422f403bdac1f58
FaithfulnessJ <faithfulnessjoshua2001@gmail.com>
2023-02-17 00:49:18 -0800

    .7-print_tebahpla.c.swp

---
+++ 0x01-variables_if_else_while/0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse, followed by a new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char a;
+
+a = 'z';
+while
+	(c >= 'a') {
+		putchar(c);
+		c--;
+	}
+putchar('\n');
+return (0);
+}
